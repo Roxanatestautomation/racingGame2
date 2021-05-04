f90a1d1c503fb263c0e4dec30e040530c1f2d39c
Roxana <roxanacroitoru378@yahoo.ro>
2021-05-04 16:47:45 +0100

    Create clases

---
+++ src/main/java/org/fasttrackit/RadioControlledCar.java
@@ -0,0 +1,11 @@
+package org.fasttrackit;
+
+public class RadioControlledCar {
+    String name;
+    String color;
+    int doorCount;
+    int wheelCount;
+    double mileage;
+    Engine engine;
+
+}
